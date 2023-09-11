f12042fb470baf1d660cc5f648cfa4ea5699914b
flare <blastclever@gmail.com>
2023-09-11 11:56:50 +0300

    feat(workspace): topbar design

--- app/(workspace)/layout.tsx
+++ app/(workspace)/layout.tsx
@@ -1,11 +1,15 @@
 import React from "react"
+import Topbar from "@/components/topbar"
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export default function Layout({children}:LayoutProps){
-  return <div className="grid grid-rows-[50px_1fr] grid-cols-1 md:grid-cols-[250px_1fr] h-full">
-   <main className="md:col-start-2 row-start-2 bg-red-200">{children}</main>
-  </div>
+  return (
+    <div className="grid grid-rows-[60px_1fr] grid-cols-1 md:grid-cols-[250px_1fr] h-screen">
+      <Topbar className="md:col-start-2 row-end" />
+      <main className="md:col-start-2 row-start-2 bg-white p-4">{children}</main>
+    </div>
+  )
 }
---
+++ components/topbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { ComponentProps } from "react";
+import { User, Switch, Button } from "@nextui-org/react";
+import { BsFillMoonFill, BsFillSunFill, BsFillGearFill } from "react-icons/bs";
+
+interface TopbarProps extends ComponentProps<"div"> {}
+
+export default function Topbar({ className, ...props }: TopbarProps) {
+  return (
+    <div
+      className={`w-full p-4 bg-white flex flex-row justify-between align-center border-b-gray-50 border-b-2 ${className}`}
+      {...props}
+    >
+      <User
+        name="Jane Doe"
+        description="Product Designer"
+        avatarProps={{
+          src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+        }}
+      />
+      <div className="flex items-center gap-2">
+        <Switch
+          startContent={<BsFillMoonFill />}
+          endContent={<BsFillSunFill />}
+        />
+        <Button size="sm" isIconOnly radius="full">
+          <BsFillGearFill />
+        </Button>
+      </div>
+    </div>
+  );
+}
