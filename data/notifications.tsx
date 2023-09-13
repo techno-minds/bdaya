feb4835d105b3161799430e3e24ae1fb2c4d53ec
flare <blastclever@gmail.com>
2023-09-14 01:21:51 +0300

    chore: clean up unncessary code related to the notifications functionality

--- data/notifications.tsx
+++ data/notifications.tsx
@@ -5,30 +5,30 @@ export interface NotificationType {
   unread?: boolean;
 }
 
-export const notifications: NotificationType[] = [
-  {
-    key: "notif1",
-    label: "Notification",
-    description: "You have received on notification",
-  },
-  {
-    key: "notif2",
-    label: "Notification",
-    description: "You have received on notification",
-  },
-  {
-    key: "notif3",
-    label: "Notification",
-    description: "You have received on notification",
-  },
-  {
-    key: "notif4",
-    label: "Notification",
-    description: "You have received on notification",
-  },
-  {
-    key: "notif5",
-    label: "Notification",
-    description: "You have received on notification",
-  },
-];
+// const notifications: NotificationType[] = [
+//   {
+//     key: "notif1",
+//     label: "Notification",
+//     description: "You have received on notification",
+//   },
+//   {
+//     key: "notif2",
+//     label: "Notification",
+//     description: "You have received on notification",
+//   },
+//   {
+//     key: "notif3",
+//     label: "Notification",
+//     description: "You have received on notification",
+//   },
+//   {
+//     key: "notif4",
+//     label: "Notification",
+//     description: "You have received on notification",
+//   },
+//   {
+//     key: "notif5",
+//     label: "Notification",
+//     description: "You have received on notification",
+//   },
+// ];
