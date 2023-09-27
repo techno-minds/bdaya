3bf6fc91beeb57dacb760eff4c9eb5e07b7a5af0
MohandTurke2002 <mohandhany35@gmail.com>
2023-09-27 17:49:11 +0300

    chore(side-bar): update side-bar in phone screen match with UI design

--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -1,34 +1,49 @@
 "use client";
-import React, { useRef, useState } from "react";
+import { useState } from "react";
 import LinkButton from "./tab-link-btn";
 import LogOut from "./log-out-btn";
 import { motion } from "framer-motion";
-import { TiThMenu } from "react-icons/ti";
-import { Button, Divider } from "@nextui-org/react";
+import { Divider } from "@nextui-org/react";
 import { useMediaQuery } from "@mantine/hooks";
 import Logo from "./Logo";
 import { routes } from "@/data/route-tabs";
 
 const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0, x: "-100%" },
+  open: {
+    opacity: 1,
+    y: 0,
+    padding: "20px",
+    height: "fit-content",
+    overflow: "visible",
+  },
+  closed: { opacity: 1, y: "100%", height: 0, padding: 0, overflow: "hidden" },
 };
 
 function SideBar() {
-  const constraintsRef = useRef(null);
-  // const matches = useMediaQuery("(max-width: 767px)");
-  // const [isOpen, setIsOpen] = useState(matches);
+  const matches = useMediaQuery("(max-width: 767px)");
+  const [isOpen, setIsOpen] = useState(matches);
   return (
-    <>
-      <motion.div
-        className="flex bg-white dark:bg-black dark:shadow-slate-700 justify-between items-start sm:relative absolute sm:top-0 sm:left-0 z-10 sm:h-screen h-fit bottom-0 gap-y-5 rounded-s-xl rounded-e-xl flex-col py-5 px-5"
-        // animate={matches ? (isOpen ? "open" : "closed") : "null"}
-        variants={variants}
-        drag="y"
-        dragConstraints={{
-          top: -50,
-          bottom: 10,
+    <div
+      className={`h-screen fixed flex justify-end items-center flex-col gap-y-3 z-20 sm:relative ${
+        isOpen ? "backdrop-blur-sm backdrop-contrast-75" : ""
+      }`}
+      onClick={() => {
+        if (isOpen) {
+          setIsOpen(false);
+        }
+      }}
+    >
+      <div
+        className="bg-zinc-500 sm:hidden w-20 h-2 relative z-30 rounded-full"
+        onClick={() => {
+          setIsOpen((isOpen) => !isOpen);
+          console.log("done");
         }}
+      ></div>
+      <motion.div
+        className="flex justify-between items-start flex-col gap-y-5 h-full z-10 bottom-0  rounded-s-xl rounded-e-xl py-5 px-5 sm:relative sm:rounded-none sm:top-0 sm:left-0 bg-white dark:bg-black dark:shadow-slate-700 "
+        animate={matches ? (isOpen ? "open" : "closed") : "null"}
+        variants={variants}
       >
         <Divider
           className="absolute top-0 hidden sm:block right-0"
@@ -51,16 +66,7 @@ function SideBar() {
         <Divider />
         <LogOut />
       </motion.div>
-      {/* <Button
-        className="flex justify-center z-30 items-center rounded-full absolute bottom-5 right-5 w-12 h-12 text-2xl sm:hidden"
-        onClick={() => setIsOpen((isOpen) => !isOpen)}
-        color="primary"
-        variant="shadow"
-        isIconOnly
-      >
-        <TiThMenu />
-      </Button> */}
-    </>
+    </div>
   );
 }
 
